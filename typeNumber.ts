f9b99655b3be319d54d8136119adf6015a542765
“minorun365” <mail@minoruonda.com>
2023-05-03 16:11:57 +0900

    いくつかファイル追加

---
+++ typeNumber.ts
@@ -0,0 +1,4 @@
+let myAge: number = 30;
+let myName: string = "20"; 
+
+console.log(myAge + Number(myName));
---
+++ typestring.ts
@@ -0,0 +1,5 @@
+const firstName: string = "AWS";
+const lastName: string = "太郎";
+console.log(
+    `私の苗字は${firstName}、名前は${lastName}です。`
+);
